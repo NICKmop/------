0427de0b6dfdd1062ceb67903c6536c655e8adf9
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-03-20 22:02:53 +0900

    commit

--- LV_pang/src/main/java/com/webSite/net/main/controller/mainController.java
+++ LV_pang/src/main/java/com/webSite/net/main/controller/mainController.java
@@ -2,12 +2,17 @@ package com.webSite.net.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class mainController {
 
-    @GetMapping("/main/main")
-    public String main(){
-        return "main/main";
+    @GetMapping("/")
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("main");
+
+        return mv;
     }
 }
